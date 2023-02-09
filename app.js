853a86e84258d1352e8a46b107c63a129f584046
Nguyen Luong Phuc <Phuc.nl200475@sis.hust.edu.vn>
2023-02-09 13:18:59 +0700

    set up server successful

---
+++ app.js
@@ -0,0 +1,54 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var accountRouter = require('./routes/account');
+var voucherRouter = require('./routes/voucher');
+var itemRouter = require('./routes/item');
+
+/* var cartRouter = require('./routes/cart');
+var orderRouter = require('./routes/order');
+var loyaltyProgramRouter = require('./routes/loyaltyProgram'); */
+
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/account', accountRouter);
+app.use('/voucher', voucherRouter);
+app.use('/item', itemRouter);
+/* app.use('/cart', cartRouter);
+app.use('/order', orderRouter);
+app.use('/loyaltyProgram', loyaltyProgramRouter); */
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;

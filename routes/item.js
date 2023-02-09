3303180de1e6b16dab41beb54172e60cd7e643b4
Nguyen Luong Phuc <109148954+KiyanoRin@users.noreply.github.com>
2023-02-09 22:19:39 +0700

    Update item.js

--- routes/item.js
+++ routes/item.js
@@ -2,11 +2,9 @@ const express = require('express');
 const router = express.Router();
 const {Item, VariantItem} = require('../models/db');
 
-// GET all accounts
+// GET all items
 router.get('/', (req, res) => {
-    Item.findAll({
-        include: VariantItem
-    })
+    Item.findAll ()
     .then(items => {
         res.status(200).json({
             message: 'Successfully retrieved all accounts',
@@ -18,6 +16,27 @@ router.get('/', (req, res) => {
         error: error
       });
     });
+});
+
+// GET one item
+router.get('/:itemId', (req, res) => {
+  Item.findAll({
+    include: [{
+      model: VariantItem,
+      where: { itemId: req.params.itemId }
+    }]
+  })
+  .then(items => {
+      res.status(200).json({
+          message: 'Successfully retrieved all accounts',
+          items
+        });
+  })
+  .catch(error => {
+    res.status(500).json({
+      error: error
+    });
+  });
 });
 
 // POST a new item
@@ -50,6 +69,38 @@ router.post('/', (req, res) => {
       }); 
   });
 
-// 
+// PUT (update) an existing account
+router.put('/', (req, res) => {
+  Item.findByPk(req.query.itemId)
+    .then(item => {
+      if (!item) {
+        return res.status(404).json({ error: 'Item not found' });
+      }
+      
+      return item.update(req.body)
+        .then(() => {
+          res.status(200).json({ message: 'Item updated successfully' });
+        });
+    })
+    .catch(error => {
+      res.status(400).json({ error: error.message });
+    });
+
+});
+
+// DELETE a specific account
+router.delete('/', (req, res) => {
+  Item.destroy({ where: { accountId: req.query.itemId } })
+    .then(() => {
+      res.status(200).json({
+        message: `Successfully deleted account with ID ${req.query.itemId}`
+      });
+    })
+    .catch(error => {
+      res.status(500).json({
+        error: error
+      });
+    });
+});
 
 module.exports = router;

90ef9284602e7b9bc9c64e9f6dc2673b96c171a5
Nguyen Luong Phuc <109148954+KiyanoRin@users.noreply.github.com>
2023-02-20 22:20:58 +0700

    Update item.js

--- routes/item.js
+++ routes/item.js
@@ -1,67 +1,31 @@
-require("dotenv").config();
-
 const express = require('express');
 const router = express.Router();
 const {Item, VariantItem, Cart, sequelize} = require('../models/db');
-const jwt = require('jsonwebtoken');
+const {verifyToken} = require('../middleware/verify');
 
-// Middleware function to verify the JWT
-const verifyToken = (req, res, next) => {
-  const token = req.header('Authorization');
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.accountId = decoded;
-    next();
-  } catch (error) {
-    return res.status(401).json({ message: 'log in please' });
-  }
-};
-
-// GET all items
-router.get('/', (req, res) => {
-    Item.findAll ()
-    .then(items => {
-        res.status(200).json({
-            message: 'Successfully retrieved all accounts',
-            items
-          });
-    })
-    .catch(error => {
-      res.status(500).json({
-        error: error
-      });
-    });
-});
-
-// add item to cart
-router.post('/:itemId',verifyToken, (req, res) => {
-  const quantity = req.body.quantity;
-  Cart.create({
-    quantity: quantity,
-    accountId: req.accountId,
-    variantItemId: req.params.itemId
-  })
-  .then(cart => {
-    res.status(201).json({
-      message: 'Successfully add item to cart',
-      cart
-    });
-  })
-  .catch(error => {
-    res.status(400).json({
-      error: error
-    });
-  }); 
-});
+const limit = parseInt(process.env.LIMIT_PAGE); // Number of items to display per page
 
 //GET by category
-router.get('/:category', (req, res) => {
-  Item.findAll ({where: {category: req.params.category}})
-    .then(items => {
-        res.status(200).json({
-            message: 'Successfully retrieved all accounts',
-            items
-          });
+router.get('/category/:category', (req, res) => {
+  const page = req.query.page || 1;
+  const offset = (page - 1) * limit;
+
+  Item.findAndCountAll({ 
+    where: {category: req.params.category},
+    offset, 
+    limit 
+  })
+    .then(result => {
+      const items = result.rows;
+      const totalItems = result.count;
+      const totalPages = Math.ceil(totalItems / limit);
+
+      res.status(200).json({
+        items,
+        currentPage: page,
+        totalPages,
+        totalItems
+      });
     })
     .catch(error => {
       res.status(500).json({
@@ -72,16 +36,10 @@ router.get('/:category', (req, res) => {
 
 // GET one item
 router.get('/:itemId', (req, res) => {
-  Item.findAll({
-    include: [{
-      model: VariantItem,
-      where: { itemId: req.params.itemId }
-    }]
-  })
-  .then(items => {
+  Item.findByPk(req.params.itemId)
+  .then(item => {
       res.status(200).json({
-          message: 'Successfully retrieved all ',
-          items
+          item
         });
   })
   .catch(error => {
@@ -91,61 +49,54 @@ router.get('/:itemId', (req, res) => {
   });
 });
 
-// POST a new item
-router.post('/', (req, res) => {
+// add item to cart
+router.post('/:itemId', verifyToken, async (req, res) => {
+  try {
+    const variantItem = await VariantItem.findByPk(req.body.itemId);
+    const quantity = req.body.quantity;
+    
+    if (variantItem.quantity < quantity) {
+      return res.status(400).json({
+        message: 'Not enough item to add to cart'
+      });
+    }
 
-    const {name, description, price, image, category, variantitems} = req.body;
-
-    Item.create({
-        name: name,
-        description: description,
-        price: price,
-        image: image,
-        category: category, 
-        VariantItems: variantitems},
-        {
-        include:[{
-            model: VariantItem
-        }]
-    })
-      .then(item => {
-        res.status(201).json({
-          message: 'Successfully created a new item',
-          item
-        });
-      })
-      .catch(error => {
-        res.status(400).json({
-          error
-        });
-      }); 
-  });
-
-// PUT (update) an existing account
-router.put('/', (req, res) => {
-  Item.findByPk(req.query.itemId)
-    .then(item => {
-      if (!item) {
-        return res.status(404).json({ error: 'Item not found' });
-      }
-      
-      return item.update(req.body)
-        .then(() => {
-          res.status(200).json({ message: 'Item updated successfully' });
-        });
-    })
-    .catch(error => {
-      res.status(400).json({ error: error.message });
+    const cart = await Cart.create({
+      quantity: quantity,
+      accountId: req.accountId,
+      variantItemId: req.params.itemId
     });
 
+    res.status(201).json({
+      message: 'Successfully add item to cart'
+    });
+  } catch (error) {
+    res.status(400).json({
+      error: error
+    });
+  }
 });
 
-// DELETE a specific account
-router.delete('/', (req, res) => {
-  Item.destroy({ where: { accountId: req.query.itemId } })
-    .then(() => {
+// GET all items
+router.get('/', (req, res) => {
+  const page = req.query.page || 0; // Default to first page if no page number is specified
+  const offset = page * limit;
+
+  Item.findAndCountAll({ 
+    offset, 
+    limit,
+    attributes: ['itemId', 'name', 'price', 'image']
+  })
+    .then(result => {
+      const items = result.rows;
+      const totalItems = result.count;
+      const totalPages = Math.ceil(totalItems / limit);
+
       res.status(200).json({
-        message: `Successfully deleted account with ID ${req.query.itemId}`
+        items,
+        currentPage: page,
+        totalPages,
+        totalItems
       });
     })
     .catch(error => {

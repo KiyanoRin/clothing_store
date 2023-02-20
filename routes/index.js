90e6196e7be7f7e06f0ad23b35007a8b2402fa88
Nguyen Luong Phuc <109148954+KiyanoRin@users.noreply.github.com>
2023-02-20 22:20:32 +0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,110 +1,73 @@
 require("dotenv").config();
-
+const bcrypt = require('bcrypt');
 const express = require('express');
 const router = express.Router();
-const {Account, User} = require('../models/db');
+const { Account, User } = require('../models/db');
 const jwt = require('jsonwebtoken');
 
-// Middleware function to verify the JWT
-const verifyToken = (req, res, next) => {
-  const token = req.header('Authorization');
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.accountId = decoded;
-    next();
-  } catch (error) {
-    return res.status(401).json({ message: 'log in please' });
-  }
-};
+router.get('/login', (req, res) => {
+  res.render('login');
+});
 
-// GET a single account by ID
-router.get('/',verifyToken, (req, res) => {
-  Account.findByPk(req.accountId)
-    .then(account => {
-      if (!account) {
-        return res.status(404).json({
-          message: `Account with ID ${req.query.accountId} not found`
+// Login
+router.post('/login', (req, res) => {
+  const { phone, password } = req.body;
+  User.findOne({ where: { phone: phone } })
+    .then(user => {
+      Account.findOne({ where: { userId: user.userId } })
+        .then(account => {
+          console.log(password);
+          bcrypt.compare(password, account.password, (err, result) => {
+            if (err) {
+              return res.status(401).json({ error: `Incorrect password` });
+            }
+            const token = jwt.sign(account.accountId, process.env.JWT_SECRET);
+            res.status(200).json(token);
+          });
+        })
+        .catch(error => {
+          res.status(500).json({ error: error.message });
         });
-      }
-      res.status(200).json({
-        message: `Successfully retrieved account with ID ${req.query.accountId}`,
-        account: account
-      });
     })
     .catch(error => {
-      res.status(500).json({
-        error: error
-      });
+      return res.status(401).json({ error: 'Phone number not found' });
     });
 });
 
+// Register
+router.post('/register', (req, res) => {
+  const { name, phone, password, email, address } = req.body;
 
-/* // POST a new account
-router.post('/', (req, res) => {
-
-   Account.create(req.body)
-    .then(account => {
-      res.status(201).json({
-        message: 'Successfully created a new account',
-        account
-      });
-    })
-    .catch(error => {
-      res.status(400).json({
-        error: error
-      });
-    }); 
-}); */
-
-// PUT (update) an existing account
-router.put('/', (req, res) => {
-  Account.findByPk(req.query.accountId)
-    .then(account => {
-      if (!account) {
-        return res.status(404).json({ error: 'Account not found' });
+  bcrypt.hash(password, 10, (err, hashedPassword) => {
+    if (err) {
+      return res.status(500).json({ error: 'Could not hash password' });
+    }
+    User.create({
+      name: name,
+      phone: phone,
+      account: {
+        password: hashedPassword,
+        email: email,
+        address: address,
+        role: `potential customer`
       }
-      
-      return account.update(req.body)
-        .then(() => {
-          res.status(200).json({ message: 'Account updated successfully' });
-        });
+    }, {
+      include: [Account]
+    })
+      .then(user => {
+        Account.findOne({ where: { userId: user.userId } })
+          .then(account => {
+            const token = jwt.sign(account.accountId, process.env.JWT_SECRET);
+            res.status(200).json(token);
+          })
+          .catch(error => {
+            res.status(500).json({ error: error.message });
+          });
     })
     .catch(error => {
       res.status(400).json({ error: error.message });
     });
-
+  });
 });
-
-
-/* // DELETE a specific account
-router.delete('/', (req, res) => {
-  Account.destroy({ where: { accountId: req.query.accountId } })
-    .then(() => {
-      res.status(200).json({
-        message: `Successfully deleted account with ID ${req.query.accountId}`
-      });
-    })
-    .catch(error => {
-      res.status(500).json({
-        error: error
-      });
-    });
-}); */
-
-/* // GET all accounts
-router.get('/', (req, res) => {
-  Account.findAll()
-    .then(accounts => {
-      res.status(200).json({
-        message: 'Successfully retrieved all accounts',
-        accounts
-      });
-    })
-    .catch(error => {
-      res.status(500).json({
-        error: error
-      });
-    });
-}); */
 
 module.exports = router;

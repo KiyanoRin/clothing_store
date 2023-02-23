924006d18559e4e33d720ca4c95a9c3c33e0a1c3
Nguyen Luong Phuc <Phuc.nl200475@sis.hust.edu.vn>
2023-02-23 23:55:49 +0700

    delete item to cart

--- routes/cart.js
+++ routes/cart.js
@@ -39,5 +39,54 @@ router.get('/', async (req, res) => {
   }
 });
 
+// delete item to cart
+/* req có dạng như sau 
+{
+  "items": [
+      {
+          "itemId": "1",
+          "size": "L"
+      },
+      {
+          "itemId":"1",
+          "size":"M"
+      },
+      {
+          "itemId":"3",
+          "size":"M"
+      }
+  ]
+} */
+router.delete("/", async (req, res) => {
+  const { items } = req.body;
+  try {
+    const promises = items.map(async item => {
+      const variantItem = await VariantItem.findOne({
+        where: {
+          itemId: item.itemId,
+          size: item.size
+        }
+      });
+
+      await Cart.destroy({
+        where: {
+          accountId: res.locals.decoded.accountId,
+          VariantItemId: variantItem.VariantItemId
+        }
+      });
+    });
+
+    await Promise.all(promises);
+
+    res.status(201).json({
+      message: "Successfully delete item to cart",
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: error.message
+    });
+  }
+});
+
 
 module.exports = router;

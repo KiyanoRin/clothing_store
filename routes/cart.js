5c6b09f77198a4d2f58496f0948e05dac8c5bfbe
Nguyen Luong Phuc <Phuc.nl200475@sis.hust.edu.vn>
2023-02-23 19:11:21 +0700

    cart

---
+++ routes/cart.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const router = express.Router();
+const {Cart, VariantItem, Item} = require('../models/db');
+
+
+// GET by accounts
+router.get('/', async (req, res) => {
+  try {
+    const carts = await Cart.findAll({
+      where: { accountId: res.locals.decoded.accountId }
+    });
+
+    const cartItems = await Promise.all(
+      carts.map(async cart => {
+        const { quantity, variantItemId } = cart;
+        const variantItem = await VariantItem.findByPk(variantItemId);
+        const item = await Item.findByPk(variantItem.itemId);
+    
+        return {
+          quantity,
+          name: item.name,
+          description: item.description,
+          price: item.price,
+          image: item.image,
+          color: variantItem.color,
+          size: variantItem.size
+        };
+      })
+    );
+    res.status(200).json({
+      cartItems
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: error.message
+    });
+  }
+});
+
+
+module.exports = router;
